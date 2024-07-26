466586f63b4e23a9e695be74e0ed1f7bcfb905c4
Tyler Wilson <tjwil0123@gmail.com>
2024-07-26 14:17:48 -0700

    test1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Sprint-4-Project
+This application is built on an exploratory analysis and visualization of a dataset of vehicles. One visualization is a scatterplot showing the relation between odometer value and price, with a checkbox allowing vehicle condition to be seen as well. The second is a histogram showing the distribution of conditions.
+
+If you would like to test this application on your own machine, simply clone the repo and run 'streamlit run app.py' on your terminal.
+
+Modules used:
+pandas
+streamlit
+plotly.express
+altair
---
+++ app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+vehicles_df = pd.read_csv('C:/Users/Tyler Wilson/Desktop/Sprint-4-Project/cleaned_vehicles.csv')
+
+# set title
+st.title('Vehicle Data Analysis')
+
+# make scatter plot
+st.header('Price vs Odometer Value')
+
+# set checkbox
+show_condition_color = st.checkbox('Color by Condition')
+
+if show_condition_color:
+    scatter_fig = px.scatter(vehicles_df, x='odometer', y='price', color='condition',
+                             title='Price vs Odometer Value (Colored by Condition)',
+                             labels={'odometer': 'Odometer (miles)', 'price': 'Price (USD)', 'condition': 'Condition'})
+else:
+    scatter_fig = px.scatter(vehicles_df, x='odometer', y='price',
+                             title='Price vs Odometer Value',
+                             labels={'odometer': 'Odometer (miles)', 'price': 'Price (USD)'})
+
+st.plotly_chart(scatter_fig)
+
+# make histogram
+st.header('Distribution of Vehicle Conditions')
+
+hist_fig = px.histogram(vehicles_df, x='condition', title='Vehicle Condition Distribution',
+                        labels={'condition': 'Condition', 'count': 'Count'})
+
+st.plotly_chart(hist_fig)
